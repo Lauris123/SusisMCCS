433c8326498bcc2284cf3ca56667c48e9928ceb1
edza <edexdev@gmail.com>
2015-04-25 19:19:50 +0300

    pieliku testu jaunu

--- Susis/Tests.cs
+++ Susis/Tests.cs
@@ -15,13 +15,24 @@ namespace Susis
         {
             // komits
             string targetResult = ".586\n.MODEL flat, stdcall\ninclude \\masm32\\include\\windows.inc\ninclude \\masm32\\macros\\macros.asm\ninclude \\masm32\\include\\masm32.inc\ninclude \\masm32\\include\\gdi32.inc\ninclude \\masm32\\include\\user32.inc\ninclude \\masm32\\include\\kernel32.inc\nincludelib\\masm32\\lib\\masm32.lib\nincludelib\\masm32\\lib\\gdi32.lib\nincludelib\\masm32\\lib\\user32.lib\nincludelib\\masm32\\lib\\kernel32.lib\ninclude \\masm32\\include\\msvcrt.inc\nincludelib \\masm32\\lib\\msvcrt.lib\r\n"
-                                  + ".data\r\nX db 3\nmov eax, input (\"beigas\")\nret\n main ENDP\nEND main";
+                                  + ".data\r\nX db 4\nmov eax, input (\"beigas\")\nret\n main ENDP\nEND main";
 
-            string input = "NOSAUKT X = 3";
+            string input = "NOSAUKT X = 4";
 
             string result = MainWindow.SuperKrutāFunkcijaKuraKompilēKodu(input);
 
             Assert.AreEqual(targetResult, result);
         }
+
+        [Test]
+        public void IzvadīšanasTests() 
+        {
+            string targetResult = ".586\n.MODEL flat, stdcall\ninclude \\masm32\\include\\windows.inc\ninclude \\masm32\\macros\\macros.asm\ninclude \\masm32\\include\\masm32.inc\ninclude \\masm32\\include\\gdi32.inc\ninclude \\masm32\\include\\user32.inc\ninclude \\masm32\\include\\kernel32.inc\nincludelib\\masm32\\lib\\masm32.lib\nincludelib\\masm32\\lib\\gdi32.lib\nincludelib\\masm32\\lib\\user32.lib\nincludelib\\masm32\\lib\\kernel32.lib\ninclude \\masm32\\include\\msvcrt.inc\nincludelib \\masm32\\lib\\msvcrt.lib\r\n"
+                                    + ".data\r\nX db 4\r\n .code\nmain PROC\r\n mov eax , X\r\nprint str$(eax)\nmov eax, input (\"beigas\")\nret\n main ENDP\nEND main"; 
+        String input = "NOSAUKT X = 4\nIZVADĪT X";
+            string result = MainWindow.SuperKrutāFunkcijaKuraKompilēKodu(input);
+            Assert.AreEqual(targetResult, result);
+        }
+
     }
 }
